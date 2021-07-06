e8f701cc3a3ae10bfb867e78ab4754c50411217f
Jason-Cee <jasoncee017@gmail.com>
2021-07-06 13:54:41 +0200

    Added functionality

--- main.py
+++ main.py
@@ -5,27 +5,63 @@ import mysql.connector
 from tkinter import *
 from tkinter import PhotoImage
 
-
+# CONNECTING TO DATABASE
 mydb = mysql.connector.connect(user='lifechoices', password='@Lifechoices1234', host='127.0.0.1',
                                database='LifeChoices_Online', auth_plugin='mysql_native_password')
 
 mycursor = mydb.cursor()
 
-
-class FirstPage:
-
-    def __init__(self, window):
-        self.window = window
-        self.window.title("Life Choices Online")
-        self.window.geometry("600x600")
-        self.window.resizable(False, False)
-        self.window.config(bg="white")
-        # IMAGE
-        self.img = PhotoImage(file="lca1.png")
-        Label(root, image=self.img).place(x=85, y=10)
-
-        self.window.mainloop()
-
-
+# WINDOW
 root = Tk()
-run = FirstPage(root)
+root.title("Lifechoices Online")  # WINDOW TITLE
+root.geometry("1000x800")  # WINDOW SIZE
+root.resizable(False, False)  # CAN'T CHANGE SIZE MANUALLY
+# root.config(bg="white")  # WINDOW COLOR
+
+# LIFECHOICES IMAGE / LOGO
+img = PhotoImage(file="lca1.png")
+Label(root, image=img).place(x=290, y=10)  # IMAGE / LOGO PLACEMENT
+
+# BLUE FRAME
+frame_left = Frame(root, width=400, height=400, bg="#346ab3")
+frame_left.place(x=100, y=200)
+log = Label(frame_left, text="LOGIN", font=("Ariel", 20), bg="#346ab3", fg="#9ccb3b")
+log.place(x=150, y=10)
+# NAME & SURNAME LABEL AND ENTRY
+user = Label(frame_left, text="Name & Surname:", font=("Ariel", 13), bg="#346ab3", fg="#9ccb3b")
+user.place(x=10, y=100)
+user_ent = Entry(frame_left, bg="#9ccb3b", fg="black")
+user_ent.place(x=200, y=101)
+
+# PASSWORD LABEL AND ENTRY
+passw = Label(frame_left, text="Password:", font=("Ariel", 13), bg="#346ab3", fg="#9ccb3b")
+passw.place(x=10, y=200)
+passw_ent = Entry(frame_left, bg="#9ccb3b", fg="black")
+passw_ent.place(x=200, y=200)
+
+# SIGN IN BUTTON
+sign = Button(frame_left, text="SIGN IN", font=("Ariel", 13), bg="#9ccb3b", fg="#346ab3")
+sign.place(x=150, y=350)
+
+
+# GIVING CLEAR BUTTON FUNCTIONALITY
+def clear():
+    user_ent.delete(0, END)
+    passw_ent.delete(0, END)
+
+
+# EXIT BUTTON AND FUNCTIONALITY
+destroy = Button(root, text="E X I T", font=("Ariel", 13), bg="#9ccb3b", fg="#346ab3")
+destroy.place(x=40, y=700)
+
+# CLEAR BUTTON
+remove = Button(frame_left, text="CLEAR", font=("Ariel", 13), bg="#9ccb3b", fg="#346ab3", command=clear)
+remove.place(x=155, y=290)
+
+# GREEN FRAME
+frame_right = Frame(root, width=400, height=400, bg="#9ccb3b")
+frame_right.place(x=500, y=200)
+reg = Label(frame_right, text="REGISTER", font=("Ariel", 20), bg="#9ccb3b", fg="#346ab3")
+reg.place(x=130, y=10)
+
+root.mainloop()
