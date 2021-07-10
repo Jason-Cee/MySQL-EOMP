ed92cd0339164006a1aaa31e2102b976a0e24856
Jason-Cee <jasoncee017@gmail.com>
2021-07-10 23:52:54 +0200

    Changes

--- admin_login.py
+++ admin_login.py
@@ -3,7 +3,6 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 
-
 root = tk.Tk()
 root.title("Admin Users Page")
 root.geometry("1054x800")
@@ -28,17 +27,17 @@ tree = ttk.Treeview(root)
 tree["columns"] = ("Name", "Id_Number", "Phone_Number", "Email_Address", "Time_in", "Time_out")
 
 # Formatting columns
-tree.column("#0", width=75, minwidth=25)
-tree.column("Name", anchor=CENTER)
-tree.column("Id_Number", anchor=CENTER, width=160)
-tree.column("Phone_Number", anchor=CENTER, width=150)
-tree.column("Email_Address", anchor=CENTER, width=150)
-tree.column("Time_in", anchor=CENTER, width=160)
-tree.column("Time_out", anchor=CENTER, width=160)
+tree.column("#0", width=55, minwidth=25)
+tree.column("Name", anchor=CENTER, width=200)
+tree.column("Id_Number", anchor=CENTER, width=154)
+tree.column("Phone_Number", anchor=CENTER, width=100)
+tree.column("Email_Address", anchor=CENTER, width=230)
+tree.column("Time_in", anchor=CENTER, width=154)
+tree.column("Time_out", anchor=CENTER, width=154)
 
 # Defining the column headings
-tree.heading("#0", text="Labels", anchor=CENTER)
-tree.heading("Name", text="Username", anchor=CENTER)
+tree.heading("#0", text="Labels", anchor=W)
+tree.heading("Name", text="Name", anchor=CENTER)
 tree.heading("Id_Number", text="ID Number", anchor=CENTER)
 tree.heading("Phone_Number", text="Mobile", anchor=CENTER)
 tree.heading("Email_Address", text="Email Address", anchor=CENTER)
@@ -55,7 +54,50 @@ for data in user:
 # Placing the treeview
 tree.place(x=0, y=60)
 
-put_in = Button(root, text="INSERT", font=("Ariel", 13), bg="black", fg="#f7f7f7")
+
+def register():
+    mydb = mysql.connector.connect(host="localhost", user="lifechoices",
+                                   password="@Lifechoices1234", database="Lifechoices_Online")
+    mycursor = mydb.cursor()
+    mycursor.execute("Select * from Login")
+    insert = ttk.Treeview(root)
+    insert['show'] = "headings"
+    # define number of columns
+    insert["columns"] = ("Name", "Id_Number", "Phone_Number", "Email_Address", "Time_in", "Time_out")
+
+    # assigning properties of columns
+    insert.column("Name", width=100, minwidth=50, anchor=CENTER)
+    insert.column("Id_Number", width=50, minwidth=100, anchor=CENTER)
+    insert.column("Phone_Number", width=50, minwidth=100, anchor=CENTER)
+    insert.column("Email_Address", width=140, minwidth=50, anchor=CENTER)
+    insert.column("Time_in", width=100, minwidth=100, anchor=CENTER)
+    insert.column("Time_out", width=100, minwidth=100, anchor=CENTER)
+
+    # heading names
+    insert.heading("Name", text="Name")
+    insert.heading("Id_Number", text="ID Number")
+    insert.heading("Phone_Number", text="Mobile")
+    insert.heading("Email_Address", text="Email")
+    insert.heading("Time_in", text="Time In")
+    insert.heading("Time_out", text="Time Out")
+
+    # For loop
+    # allows for the records in "SignInOutTable"
+    # to be displayed in the treeview diagram in correct order
+    i = 0
+    for user in mycursor:
+        insert.insert('', i, text="", values=(user[0], user[1], user[2], user[3], user[4], user[5]))
+        i = +1
+
+    v_scroll = ttk.Scrollbar(insert, orient="vertical")
+    v_scroll.configure(command=insert.yview)
+    insert.configure(yscrollcommand=v_scroll.set)
+    v_scroll.pack(fill=Y, side=RIGHT)
+
+    insert.place(x=50, y=70, width=1000, height=200)
+
+
+put_in = Button(root, text="INSERT", font=("Ariel", 13), bg="black", fg="#f7f7f7", command=register)
 put_in.place(x=250, y=350)
 
 take_out = Button(root, text="DELETE", font=("Ariel", 13), bg="black", fg="#f7f7f7")
